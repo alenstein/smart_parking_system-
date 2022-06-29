75a8e3218bb0ebae27cb89790d885b27d97af066
Jose <willtjose@gmail.com>
2022-06-29 17:22:49 +0200

    change by jose

--- app/src/main/java/com/example/smart_parking/SignUpLogin.java
+++ app/src/main/java/com/example/smart_parking/SignUpLogin.java
@@ -2,19 +2,121 @@ package com.example.smart_parking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 
 public class SignUpLogin extends AppCompatActivity {
     private Button SignIn;
 
+    private EditText email;
+    private EditText password;
+    private Button signIn, registerSwitchText;
+    private TextView forgotPasswordText;
+
+    private FirebaseAuth auth;
+    private FirebaseDatabase db;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SignIn = findViewById(R.id.signInButton);
         setContentView(R.layout.activity_sign_up_login);
+
+        initComponents();
+        attachListeners();
+
     }
-    
+
+
+
+    private void initComponents() {
+
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        signIn=findViewById(R.id.signInButton);
+        registerSwitchText=findViewById(R.id.signUpButton);
+        forgotPasswordText=findViewById(R.id.forgotPassword);
+
+        auth=FirebaseAuth.getInstance();
+
+    }
+
+    private void attachListeners() {
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email=email.getText().toString();
+                String txt_password=password.getText().toString();
+                if(TextUtils.isEmpty(txt_email)){
+                    Toast.makeText(SignUpLogin.this,"Email can't be blank!",Toast.LENGTH_SHORT).show();
+                }else if(TextUtils.isEmpty(txt_password)){
+                    Toast.makeText(SignUpLogin.this,"Password can't be blank!",Toast.LENGTH_SHORT).show();
+                }else{
+                    //if email and password are not empty log the user
+                    loginUser(txt_email, txt_password);
+                }
+
+            }
+        });
+
+        registerSwitchText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // register the user
+                startActivity(new Intent(SignUpLogin.this, registerUser.class) );
+
+            }
+        });
+
+        forgotPasswordText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //call class to handle forgot password
+                startActivity(new Intent(SignUpLogin.this, Forgot_Password_Activity.class) );
+            }
+        });
+    }
+    private void loginUser(String email, String password) {
+        //method user in logging in the user
+        final Task<AuthResult> authResultTask;
+        authResultTask = auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //start the MainActivity
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(SignUpLogin.this, "Invalid Credentials!", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    };
 
 }
+
+
+
+
+
